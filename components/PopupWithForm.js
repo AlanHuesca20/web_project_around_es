03cd6fe28bbe8a9dc6111d50f424c1d4ec1ddcab
Alan Huesca Salinas <alanhuesca2002@gmail.com>
2026-06-16 20:53:03 -0600

    Agrega JS11.2

---
+++ components/PopupWithForm.js
@@ -0,0 +1,41 @@
+import { Popup } from "./Popup.js";
+
+export class PopupWithForm extends Popup {
+  constructor(popupSelector, handleFormSubmit) {
+    super(popupSelector);
+    this._handleFormSubmit = handleFormSubmit;
+
+    this._form = this._popup.querySelector(".popup__form");
+    this._submitButton = this._form.querySelector(".popup__button");
+    this._defaultButtonText = this._submitButton.textContent;
+  }
+
+  _getInputValues() {
+    const inputList = this._form.querySelectorAll(".popup__input");
+    const formValues = {};
+
+    inputList.forEach((input) => {
+      formValues[input.name] = input.value;
+    });
+
+    return formValues;
+  }
+
+  setEventListeners() {
+    super.setEventListeners();
+
+    this._form.addEventListener("submit", (evt) => {
+      evt.preventDefault();
+      this.renderLoading(true);
+
+      this._handleFormSubmit(this._getInputValues()).finally(() =>
+        this.renderLoading(false),
+      );
+    });
+  }
+
+  close() {
+    super.close();
+    this._form.reset();
+  }
+}
